51c9056289f6926eda6231ebec8269051b84e3d2
00day0 <therandomuser11@gmail.com>
2020-01-01 21:27:32 +0100

    OneUI-like settings actionbar [2/3] todo: add settings observer so you won't need to restart settings app to toggle it
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- src/com/android/settings/core/SettingsBaseActivity.java
+++ src/com/android/settings/core/SettingsBaseActivity.java
@@ -29,6 +29,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.ArraySet;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,6 +53,9 @@ public class SettingsBaseActivity extends FragmentActivity {
     protected static final boolean DEBUG_TIMING = false;
     private static final String TAG = "SettingsBaseActivity";
     private static final String DATA_SCHEME_PKG = "package";
+
+    View baseSpacer;
+    View baseMainLayout;
 
     // Serves as a temporary list of tiles to ignore until we heard back from the PM that they
     // are disabled.
@@ -87,9 +91,30 @@ public class SettingsBaseActivity extends FragmentActivity {
         }
         setActionBar(toolbar);
 
+        baseSpacer = findViewById(R.id.settings_submenu_spacer);
+        baseMainLayout = findViewById(R.id.base_content_scrollable_container);
+
+        if (!isBaseSpacerEnabled() && baseSpacer != null && baseMainLayout != null) {
+            baseSpacer.setVisibility(View.GONE);
+            setMargins(baseMainLayout, 0,getResources().getDimensionPixelSize(R.dimen.homepage_spacer_off_margin),0,0);
+        }
+
         if (DEBUG_TIMING) {
             Log.d(TAG, "onCreate took " + (System.currentTimeMillis() - startTime)
                     + " ms");
+        }
+    }
+
+    private boolean isBaseSpacerEnabled() {
+        return Settings.System.getInt(this.getContentResolver(),
+        Settings.System.SETTINGS_SPACER, 1) != 0;
+    }
+
+    private static void setMargins (View v, int l, int t, int r, int b) {
+        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
+            p.setMargins(l, t, r, b);
+            v.requestLayout();
         }
     }
 
