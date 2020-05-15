5d3bc45297b86425b893363110d53e09c4ddeb54
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-05-15 22:51:00 +0700

    Settings:Fix OTA

--- src/com/android/settings/system/SystemUpdatePreferenceController.java
+++ src/com/android/settings/system/SystemUpdatePreferenceController.java
@@ -35,7 +35,7 @@ public class SystemUpdatePreferenceController extends BasePreferenceController {
     private static final String KEY_SYSTEM_UPDATE_SETTINGS = "system_update_settings";
 
     private static final String OTA_BUILD_TYPE_PROP = "ro.cherish.build_type";
-    private static final String OTA_APP_PACKAGE = "ro.cherish.ota";
+    private static final String OTA_APP_PACKAGE = "com.cherish.ota";
 
     private final UserManager mUm;
 
