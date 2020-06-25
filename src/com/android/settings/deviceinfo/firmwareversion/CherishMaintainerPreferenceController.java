73b643443d5adf23cd3d54fee792ceb56685c26b
Jackeagle <jackeagle102@gmail.com>
2020-06-25 10:52:51 +0200

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="cherish_maintainer"></string>
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Id1d320fe75ab97d786e6e3ae6417c6237497698d

--- src/com/android/settings/deviceinfo/firmwareversion/CherishMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/CherishMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class CherishMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "CherishMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.cherish.maintainer";
 
     public CherishMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class CherishMaintainerPreferenceController extends BasePreferenceControl
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.cherish_maintainer);
+        return maintainer;
     }
 }
