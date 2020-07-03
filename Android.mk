351be0cafeebd3a97e2b24d54d627041ecff7fa2
rohan <purohit.rohan@gmail.com>
2020-07-03 04:48:19 -0400

    Settings: import airbnb-lottie
    Change-Id: I4f2c38ff15a6bb663379d1aae551748c42688630

--- Android.mk
+++ Android.mk
@@ -35,6 +35,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview \
     com.google.android.material_material \
     setupcompat \
+    airbnb-lottie \
     setupdesign
 
 LOCAL_JAVA_LIBRARIES := \
@@ -52,7 +53,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     contextualcards \
     settings-logtags \
     zxing-core-1.7 \
-    faceunlock_utils
+    faceunlock_utils \
+    airbnb-lottie
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
     frameworks/support/v7/preference/res \
