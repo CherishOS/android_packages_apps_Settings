4c29f9a5c30c4e3431f5ec76515f2a63a2a50d46
IacobIonut01 <paulionut2003@gmail.com>
2020-10-08 14:03:21 +0300

    PowerUsageSummary : Use Custom Header Controller for new BatteryMeterView
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: Ice6063c3a9533d4ad2ac94c7cff43ea775a820ad
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

---
+++ src/com/android/settings/fuelgauge/BatteryHeaderPreferenceController.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2020 The dotOS Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+package com.android.settings.fuelgauge;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.icu.text.NumberFormat;
+import android.os.BatteryManager;
+import android.os.PowerManager;
+import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.overlay.FeatureFactory;
+import com.android.settings.utils.AnnotationSpan;
+import com.android.settings.widget.EntityHeaderController;
+import com.android.settingslib.HelpUtils;
+import com.android.settingslib.Utils;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+import com.android.settingslib.core.lifecycle.LifecycleObserver;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.widget.LayoutPreference;
+
+/**
+ * Controller that update the battery header view for dot BatteryMeterView
+ */
+public class BatteryHeaderPreferenceController extends BasePreferenceController
+        implements PreferenceControllerMixin, LifecycleObserver, OnStart,
+        BatteryPreferenceController {
+    @VisibleForTesting
+    TextView mSummary1;
+    @VisibleForTesting
+    static final String KEY_BATTERY_HEADER = "battery_header";
+    private static final String ANNOTATION_URL = "url";
+
+    private Activity mActivity;
+    private PreferenceFragmentCompat mHost;
+    private Lifecycle mLifecycle;
+
+    private LayoutPreference mBatteryLayoutPref;
+
+    public BatteryHeaderPreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    public void setActivity(Activity activity) {
+        mActivity = activity;
+    }
+
+    public void setFragment(PreferenceFragmentCompat fragment) {
+        mHost = fragment;
+    }
+
+    public void setLifecycle(Lifecycle lifecycle) {
+        mLifecycle = lifecycle;
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mBatteryLayoutPref = screen.findPreference(getPreferenceKey());
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return AVAILABLE_UNSEARCHABLE;
+    }
+
+    @Override
+    public void onStart() {
+        EntityHeaderController.newInstance(mActivity, mHost,
+                mBatteryLayoutPref.findViewById(R.id.battery_entity_header))
+                .setRecyclerView(mHost.getListView(), mLifecycle)
+                .styleActionBar(mActivity);
+    }
+    private CharSequence generateLabel(BatteryInfo info) {
+        if (BatteryUtils.isBatteryDefenderOn(info)) {
+            return null;
+        } else if (info.remainingLabel == null) {
+            return info.statusLabel;
+        } else {
+            return info.remainingLabel;
+        }
+    }
+
+    /**
+     * Callback which receives text for the summary line.
+     */
+    public void updateBatteryStatus(String label, BatteryInfo info) {
+        mSummary1.setText(label != null ? label : generateLabel(info));
+    }
+}
