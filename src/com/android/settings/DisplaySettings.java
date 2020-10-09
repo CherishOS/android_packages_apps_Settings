f132fff67fcc907bbcead3c5dbbba8cf356b3b5c
Jyotiraditya <imjyotiraditya@pm.me>
2020-10-09 10:39:59 +0530

    Settings: Move blur toggle to Display options.
    Change-Id: I64cadfd69c66a30ada9f5ba5509dd97a85124326
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -28,6 +28,7 @@ import com.android.settings.display.PocketJudgePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
+import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
 import com.android.settings.display.EmulateDisplayCutoutPreferenceController;
@@ -81,6 +82,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new PocketJudgePreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new EnableBlursPreferenceController(context));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
