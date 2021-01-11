Wave-Project/packages_apps_Settings

Settings: Revamp settings dashboard
Change-Id: I05ec31cfe6c84cc3b1c9dbaf1acdc8b993f483dd
Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -31,7 +31,6 @@ import com.android.settings.R;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.support.SupportPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -50,7 +49,7 @@ public class TopLevelSettings extends DashboardFragment implements
 
     @Override
     protected int getPreferenceScreenResId() {
-        return R.xml.top_level_settings;
+        return R.xml.top_level_settings_wave;
     }
 
     @Override
@@ -66,7 +65,6 @@ public class TopLevelSettings extends DashboardFragment implements
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        use(SupportPreferenceController.class).setActivity(getActivity());
     }
 
     @Override
@@ -100,7 +98,7 @@ public class TopLevelSettings extends DashboardFragment implements
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.top_level_settings) {
+            new BaseSearchIndexProvider(R.xml.top_level_settings_wave) {
 
                 @Override
                 protected boolean isPageSearchEnabled(Context context) {

CherishOS/android_packages_apps_Settings

Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -31,7 +31,6 @@ import com.android.settings.R;
 import com.android.settings.core.SubSettingLauncher;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settings.support.SupportPreferenceController;
 import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.search.SearchIndexable;
 
@@ -50,7 +49,7 @@ public class TopLevelSettings extends DashboardFragment implements
 
     @Override
     protected int getPreferenceScreenResId() {
-        return R.xml.top_level_settings;
+        return R.xml.top_level_settings_cherish;
     }
 
     @Override
@@ -66,7 +65,6 @@ public class TopLevelSettings extends DashboardFragment implements
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        use(SupportPreferenceController.class).setActivity(getActivity());
     }
 
     @Override
@@ -100,7 +98,7 @@ public class TopLevelSettings extends DashboardFragment implements
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.top_level_settings) {
+            new BaseSearchIndexProvider(R.xml.top_level_settings_cherish) {
 
                 @Override
                 protected boolean isPageSearchEnabled(Context context) {