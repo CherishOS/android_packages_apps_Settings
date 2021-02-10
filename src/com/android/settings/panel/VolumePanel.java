1bf8445abb0f130a2764f2cc1ee54c23735dbb88
Hưng Phan <phandinhhungvp2001@gmail.com>
2021-02-10 16:30:32 +0700

    [SQUASH]Remove Adaptive Playback
    Revert "AdaptivePlayback: Remove mVideoPaused from VideoPreference"
    
    This reverts commit 7e2d5fa9e493ee42473d49b475680d50fc85878c.
    
    Revert "Settings: Add switch for linked ring and media notification volumes"
    
    This reverts commit ac282301bdbf728ccf605fec98cb99cf74074ecb.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Revert "Settings: Add footer explanation to Adaptive Playback"
    
    This reverts commit be9de5b189bd1eb05f3e41104b1a6e635690ba4a.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    
    Revert "Settings: Adaptive Playback: allow choosing no timeout."
    
    This reverts commit a3faa5485e8fa054de663201edac3b44076d2656.
    
    Revert "Settings: Introduce Adaptive Playback [2/2]"
    
    This reverts commit aaa14835303c7a88db9f534ce5aaf99c4ab7ad98.
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/panel/VolumePanel.java
+++ src/com/android/settings/panel/VolumePanel.java
@@ -21,7 +21,6 @@ import static com.android.settings.slices.CustomSliceRegistry.REMOTE_MEDIA_SLICE
 import static com.android.settings.slices.CustomSliceRegistry.VOLUME_ALARM_URI;
 import static com.android.settings.slices.CustomSliceRegistry.VOLUME_CALL_URI;
 import static com.android.settings.slices.CustomSliceRegistry.VOLUME_MEDIA_URI;
-import static com.android.settings.slices.CustomSliceRegistry.VOLUME_NOTIFICATION_URI;
 import static com.android.settings.slices.CustomSliceRegistry.VOLUME_RINGER_URI;
 
 import android.app.settings.SettingsEnums;
@@ -31,7 +30,6 @@ import android.net.Uri;
 import android.provider.Settings;
 
 import com.android.settings.R;
-import com.android.settings.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,10 +60,6 @@ public class VolumePanel implements PanelContent {
         uris.add(MEDIA_OUTPUT_INDICATOR_SLICE_URI);
         uris.add(VOLUME_CALL_URI);
         uris.add(VOLUME_RINGER_URI);
-        if (Utils.isVoiceCapable(mContext) && Settings.Secure.getInt(
-                mContext.getContentResolver(), Settings.Secure.VOLUME_LINK_NOTIFICATION, 1) == 0) {
-            uris.add(VOLUME_NOTIFICATION_URI);
-        }
         uris.add(VOLUME_ALARM_URI);
         return uris;
     }
