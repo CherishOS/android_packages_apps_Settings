6ab2252c8234adb589a86564eabacad462180ecf
Elluzion <dyrex2004@gmail.com>
2021-03-06 17:20:17 +0100

    Fuelgauge: Introduce NXT UI
    - Also allow kotlin in src
    
    Change-Id: Ie792d76fb5ecc82ebad53e9696ebe445ef1bd484
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/fuelgauge/BatteryHeaderPreferenceController.java
+++ src/com/android/settings/fuelgauge/BatteryHeaderPreferenceController.java
@@ -25,16 +25,26 @@ import android.icu.text.NumberFormat;
 import android.os.BatteryManager;
 import android.os.PowerManager;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
+import androidx.core.content.ContextCompat;
 
 import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settings.fuelgauge.batterytip.BatteryTipPolicy;
+import com.android.settings.overlay.FeatureFactory;
+import com.android.settings.utils.AnnotationSpan;
 import com.android.settings.widget.EntityHeaderController;
+import com.android.settings.widget.ProgressCard;
+import com.android.settingslib.HelpUtils;
 import com.android.settingslib.Utils;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
@@ -45,27 +55,31 @@ import com.android.settingslib.widget.LayoutPreference;
  * Controller that update the battery header view
  */
 public class BatteryHeaderPreferenceController extends BasePreferenceController
-        implements PreferenceControllerMixin, LifecycleObserver, OnStart {
+        implements PreferenceControllerMixin, LifecycleObserver, OnStart,
+        BatteryPreferenceController {
     @VisibleForTesting
     static final String KEY_BATTERY_HEADER = "battery_header";
+    private static final String ANNOTATION_URL = "url";
 
     @VisibleForTesting
-    BatteryMeterView mBatteryMeterView;
+    BatteryStatusFeatureProvider mBatteryStatusFeatureProvider;
     @VisibleForTesting
-    TextView mBatteryPercentText;
-    @VisibleForTesting
-    TextView mSummary1;
+    ProgressCard mBatteryInfoCard;
 
     private Activity mActivity;
     private PreferenceFragmentCompat mHost;
     private Lifecycle mLifecycle;
     private final PowerManager mPowerManager;
-
+    private BatteryTipPolicy mPolicy;
     private LayoutPreference mBatteryLayoutPref;
+
+    private boolean progressShouldAnimate = true;
 
     public BatteryHeaderPreferenceController(Context context, String key) {
         super(context, key);
         mPowerManager = context.getSystemService(PowerManager.class);
+        mBatteryStatusFeatureProvider = FeatureFactory.getFactory(context)
+                .getBatteryStatusFeatureProvider(context);
     }
 
     public void setActivity(Activity activity) {
@@ -84,12 +98,13 @@ public class BatteryHeaderPreferenceController extends BasePreferenceController
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mBatteryLayoutPref = screen.findPreference(getPreferenceKey());
-        mBatteryMeterView = mBatteryLayoutPref
-                .findViewById(R.id.battery_header_icon);
-        mBatteryPercentText = mBatteryLayoutPref.findViewById(R.id.battery_percent);
-        mSummary1 = mBatteryLayoutPref.findViewById(R.id.summary1);
+        mBatteryInfoCard = mBatteryLayoutPref.findViewById(R.id.battery_info_card);
 
-        quickUpdateHeaderPreference();
+        if (com.android.settings.Utils.isBatteryPresent(mContext)) {
+            quickUpdateHeaderPreference();
+        } else {
+            showHelpMessage();
+        }
     }
 
     @Override
@@ -105,31 +120,60 @@ public class BatteryHeaderPreferenceController extends BasePreferenceController
                 .styleActionBar(mActivity);
     }
 
-    public void updateHeaderPreference(BatteryInfo info) {
-        mBatteryPercentText.setText(formatBatteryPercentageText(info.batteryLevel));
-        if (info.remainingLabel == null) {
-            mSummary1.setText(info.statusLabel);
+    private String generateLabel(BatteryInfo info) {
+        if (BatteryUtils.isBatteryDefenderOn(info)) {
+            return null;
+        } else if (info.remainingLabel == null) {
+            return info.statusLabel.toString();
         } else {
-            mSummary1.setText(info.remainingLabel);
+            return info.remainingLabel.toString();
         }
+    }
 
-        mBatteryMeterView.setBatteryLevel(info.batteryLevel);
-        mBatteryMeterView.setCharging(!info.discharging);
-        mBatteryMeterView.setPowerSave(mPowerManager.isPowerSaveMode());
+    public void updateHeaderPreference(BatteryInfo info) {
+        mBatteryInfoCard.setTitle(formatBatteryPercentageText(info.batteryLevel).toString());
+        if (!mBatteryStatusFeatureProvider.triggerBatteryStatusUpdate(this, info)) {
+            mBatteryInfoCard.setSummary(generateLabel(info));
+        }
+    }
+
+    /**
+     * Callback which receives text for the summary line.
+     */
+    public void updateBatteryStatus(String label, BatteryInfo info) {
+        mBatteryInfoCard.setSummary(label != null ? label : generateLabel(info));
     }
 
     public void quickUpdateHeaderPreference() {
         Intent batteryBroadcast = mContext.registerReceiver(null,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
         final int batteryLevel = Utils.getBatteryLevel(batteryBroadcast);
-        final boolean discharging =
-                batteryBroadcast.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) == 0;
 
-        // Set battery level and charging status
-        mBatteryMeterView.setBatteryLevel(batteryLevel);
-        mBatteryMeterView.setCharging(!discharging);
-        mBatteryMeterView.setPowerSave(mPowerManager.isPowerSaveMode());
-        mBatteryPercentText.setText(formatBatteryPercentageText(batteryLevel));
+        // Set charging status
+        mBatteryInfoCard.setProgress(batteryLevel, progressShouldAnimate);
+        progressShouldAnimate = false;
+        mBatteryInfoCard.setTitle(formatBatteryPercentageText(batteryLevel).toString());
+        if (mPowerManager.isPowerSaveMode()) {
+            mBatteryInfoCard.setTitleTint(
+                Utils.getColorAttrDefaultColor(mContext, android.R.attr.colorForeground));
+        } else if (batteryLevel <= 15) {
+            mBatteryInfoCard.setTitleTint(
+                mContext.getColor(R.color.battery_icon_color_error));
+        } else {
+            mBatteryInfoCard.setTitleTint(
+                Utils.getColorAttrDefaultColor(mContext, android.R.attr.colorAccent));
+        }
+    }
+
+    @VisibleForTesting
+    void showHelpMessage() {
+        mBatteryInfoCard.setTitle(mContext.getText(R.string.unknown).toString());
+        // Add linkable text for learn more
+        final Intent helpIntent = HelpUtils.getHelpIntent(mContext,
+                mContext.getString(R.string.help_url_battery_missing),
+                mContext.getClass().getName());
+        final AnnotationSpan.LinkInfo linkInfo = new AnnotationSpan
+                .LinkInfo(mContext, ANNOTATION_URL, helpIntent);
     }
 
     private CharSequence formatBatteryPercentageText(int batteryLevel) {
