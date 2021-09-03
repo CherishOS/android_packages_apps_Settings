ea5b05508e91418d7443d66b9545b154c511a061
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-09-04 01:12:26 +0530

    Settings: Default device name to marketname if available
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
+++ src/com/android/settings/deviceinfo/DeviceNamePreferenceController.java
@@ -22,6 +22,7 @@ import android.net.wifi.SoftApConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.text.SpannedString;
 
@@ -82,7 +83,7 @@ public class DeviceNamePreferenceController extends BasePreferenceController
         mDeviceName = Settings.Global.getString(mContext.getContentResolver(),
                 Settings.Global.DEVICE_NAME);
         if (mDeviceName == null) {
-            mDeviceName = Build.MODEL;
+            mDeviceName = SystemProperties.get("ro.product.marketname", Build.MODEL);
         }
     }
 
