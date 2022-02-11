43c5ff1e70c0b41bdaf0184b0b7a9dfce84ee2a9
DennySPb <dennyspb@gmail.com>
2022-02-11 16:13:24 +0300

    Settings: Option to change PowerUsage provider
        * Google or AOSP one
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/google/android/settings/overlay/FeatureFactoryImpl.java
+++ src/com/google/android/settings/overlay/FeatureFactoryImpl.java
@@ -1,10 +1,36 @@
 package com.google.android.settings.overlay;
 
+import android.content.Context;
+
+import android.os.SystemProperties;
+
+import com.android.settings.fuelgauge.PowerUsageFeatureProvider;
+import com.android.settings.fuelgauge.PowerUsageFeatureProviderImpl;
+import com.google.android.settings.fuelgauge.PowerUsageFeatureProviderGoogleImpl;
+
 import com.android.settings.accounts.AccountFeatureProvider;
 import com.google.android.settings.accounts.AccountFeatureProviderGoogleImpl;
 
 public final class FeatureFactoryImpl extends com.android.settings.overlay.FeatureFactoryImpl {
-    private AccountFeatureProvider mAccountFeatureProvider;
+
+    private PowerUsageFeatureProvider mPowerUsageFeatureProvider; AccountFeatureProvider mAccountFeatureProvider;
+
+    private final boolean isPowerUsageProviderGoogle =
+                                  SystemProperties.getBoolean("persist.powerusage_provider_google" , true);
+
+    @Override
+    public PowerUsageFeatureProvider getPowerUsageFeatureProvider(Context context) {
+        if (mPowerUsageFeatureProvider == null) {
+            if (isPowerUsageProviderGoogle) {
+                mPowerUsageFeatureProvider = new PowerUsageFeatureProviderGoogleImpl(
+                        context.getApplicationContext());
+            } else {
+                 mPowerUsageFeatureProvider = new PowerUsageFeatureProviderImpl(
+                        context.getApplicationContext());
+            }
+        }
+        return mPowerUsageFeatureProvider;
+    }
 
     @Override
     public AccountFeatureProvider getAccountFeatureProvider() {
