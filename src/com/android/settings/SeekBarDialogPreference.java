c7aa0ee52eefa12b8c33da41ffba50bd6e23e917
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-10 23:05:20 +0800

    Settings: add TextView for summary into SeekBarDialogPreference
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>
    Change-Id: Icff728749c3e597a4127d77646bdf3a742c35d83
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/SeekBarDialogPreference.java
+++ src/com/android/settings/SeekBarDialogPreference.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2007 The Android Open Source Project
+ *           (C) 2022 Project Kaleidoscope
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
