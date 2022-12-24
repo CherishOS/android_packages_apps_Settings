3aa663ccdb8d2b7e55ee55cd3de580c4d51c217e
minaripenguin <minaripenguin@users.noreply.github.com>
2022-12-25 07:44:07 +0800

    Settings: Show Flavor and Android version
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDetailPreferenceController.java
@@ -73,7 +73,7 @@ public class FirmwareVersionDetailPreferenceController extends BasePreferenceCon
 
     @Override
     public CharSequence getSummary() {
-        return Build.VERSION.RELEASE_OR_PREVIEW_DISPLAY;
+        return "Tiramisu"  + " | " + "13";
     }
 
     @Override
